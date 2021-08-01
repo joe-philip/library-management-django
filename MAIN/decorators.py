ca5c96a982622a5dde376fa5794808f8c6390020
Joe Philip <joe.philip@hotmail.co.in>
2021-08-02 00:37:29 +0530

    Added custom decorator function for checking unauthenticated user

---
+++ MAIN/decorators.py
@@ -0,0 +1,10 @@
+from django.shortcuts import redirect
+
+
+def unauthenticatedUser(view_function):
+    def wrapper_function(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/')
+        else:
+            return view_function(request, *args, **kwargs)
+    return wrapper_function
