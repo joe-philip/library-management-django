5218b047f9f4877cf388d37c47371e4a4b86904f
Joe Philip <joe.philip@hotmail.co.in>
2021-08-02 00:11:23 +0530

    Made configurations for admin app

--- LIBRARY/settings.py
+++ LIBRARY/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'MAIN'
+    'MAIN',
+    'ADMIN'
 ]
 
 AUTH_USER_MODEL = 'MAIN.User'
--- LIBRARY/urls.py
+++ LIBRARY/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('MAIN.urls'))
+    path('', include('MAIN.urls')),
+    path('ADMIN/', include('ADMIN.urls'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
