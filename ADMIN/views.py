b03569dcb5768931852e1cc1f04d23ebd77a15fc
Joe Philip <joe.philip@hotmail.co.in>
2021-08-02 01:04:18 +0530

    added security check for admin dashboard with sessions

--- ADMIN/views.py
+++ ADMIN/views.py
@@ -1,9 +1,17 @@
 from django.http.response import HttpResponse
 from ADMIN.decorators import checkAdmin
+from MAIN.models import User
 
 # Create your views here.
 
 
 @checkAdmin
 def dashboard(request):
-    return HttpResponse('This is admin dashboard')
+    if request.session.has_key('email'):
+        user = User.objects.get(email=request.session['email'])
+        if user.account_type == 'admin':
+            return HttpResponse('This is admin dashboard')
+        else:
+            return HttpResponse('Sorry you are not authorized to access this page<br><a href="/">Go Home</a>')
+    else:
+        return HttpResponse('Login to access this page<br><a href="/login">Go Home</a>')
