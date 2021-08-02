bdb889fb2d1333c5a3e5af191b0da2b461818219
Joe Philip <joe.philip@hotmail.co.in>
2021-08-03 02:09:34 +0530

    Modified admin dashboard to display list of librarians

--- ADMIN/functions.py
+++ ADMIN/functions.py
@@ -3,6 +3,10 @@ from MAIN.models import User
 
 def getAdminDashboard(userObject):
     context = {
-        'user': userObject,
+        'user': userObject
     }
+    try:
+        context['librarians']=User.objects.filter(account_type='librarian')
+    except:
+        pass
     return context
