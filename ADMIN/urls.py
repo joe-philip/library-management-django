9440444ce8346a2b8da893339105f8d1d5ad24d9
Joe Philip <joe.philip@hotmail.co.in>
2021-08-06 03:13:04 +0530

    Defined editprofile() and linked it with dashboard page

--- ADMIN/urls.py
+++ ADMIN/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     path('activate/<int:id>', views.activate, name='activate'),
     path('deactivate/<int:id>', views.deactivate, name='deactivate'),
+    path('editprofile', views.editprofile, name='editprofile'),
     path('delete/<int:id>', views.delete, name='delete')
 ]
