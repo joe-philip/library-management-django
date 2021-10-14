3c277e1c59f1eb496fbe5f080fec91c1d5df0ae0
Joe Philip <joe.philip@hotmail.co.in>
2021-10-14 10:53:13 +0530

    defined models for books, authors, publishers, category in LIBRARIAN app

--- LIBRARIAN/admin.py
+++ LIBRARIAN/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from LIBRARIAN.models import authors, publishers, book_category, books
 
 # Register your models here.
+admin.site.register(authors)
+admin.site.register(publishers)
+admin.site.register(book_category)
+admin.site.register(books)
--- LIBRARIAN/models.py
+++ LIBRARIAN/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+
+
+class books(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(
+        "LIBRARIAN.book_category",
+        on_delete=models.CASCADE,
+        null=True)
+    publisher = models.ForeignKey(
+        "LIBRARIAN.publishers",
+        on_delete=models.CASCADE,
+        null=True)
+    author = models.ForeignKey(
+        "LIBRARIAN.authors",
+        on_delete=models.CASCADE,
+        null=True)
+    time_added = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        verbose_name_plural = "Books"
+        verbose_name = "Book"
+
+    def __str__(self) -> str:
+        return f'{self.id}'
+
+
+class authors(models.Model):
+    name = models.CharField(max_length=50)
+    author_info = models.CharField(max_length=500, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Authors"
+        verbose_name = "Author"
+
+    def __str__(self) -> str:
+        return f'{self.id}'
+
+
+class publishers(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Publishers"
+        verbose_name = "Publisher"
+
+    def __str__(self) -> str:
+        return f'{self.id}'
+
+
+class book_category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Book Categories"
+        verbose_name = "Book Category"
+
+    def __str__(self):
+        return self.name
